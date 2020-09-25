3b56d1f0355a966ff33442a947a217b4e33790d1
CNikolik <71856457+CNikolik@users.noreply.github.com>
2020-09-25 12:28:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ info.onCountdownEnd(function () {
         enemySprite.setPosition(randint(0, 160), 0)
         enemySprite.setVelocity(0, 1)
         enemySprite.follow(playerSprite)
-        enemySprite.lifespan = 3500
+        enemySprite.lifespan = 5000
         info.startCountdown(250 / gameSpeed + 0.25)
     }
 })
@@ -36,15 +36,6 @@ info.onLifeZero(function () {
     game.over(false, effects.dissolve)
     game.reset()
 })
-function setFacing (sprite: Sprite, left: Image, right: Image, center: Image) {
-    if (sprite.vx < 0) {
-        sprite.setImage(left)
-    } else if (sprite.vx > 0) {
-        sprite.setImage(right)
-    } else {
-        sprite.setImage(center)
-    }
-}
 sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
     info.changeScoreBy(1)
 })
@@ -196,7 +187,7 @@ function start () {
         `, SpriteKind.Player)
     playerSprite.setPosition(0, 120)
     playerSprite.setFlag(SpriteFlag.StayInScreen, true)
-    controller.moveSprite(playerSprite, 200, 200)
+    controller.moveSprite(playerSprite)
     info.startCountdown(1)
     gameState = 1
 }
@@ -204,7 +195,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     otherSprite.destroy()
     info.changeLifeBy(-1)
 })
-let sprite_list: Sprite[] = []
 let gameSpeed = 0
 let playerSprite: Sprite = null
 let enemySprite: Sprite = null
@@ -331,81 +321,72 @@ scene.setBackgroundImage(img`
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
     `)
-let notes = [
-262,
-294,
-330,
-392,
-440,
-523
-]
-music.setVolume(255)
 gameState = 0
+while (true) {
+    music.playMelody("E B C5 A B G A F ", 197)
+}
 game.onUpdate(function () {
     if (gameState == 1) {
-        setFacing(playerSprite, sprites.castle.heroWalkShieldSideLeft1, sprites.castle.heroWalkSideRight2, sprites.castle.heroWalkFront1)
-        sprite_list = sprites.allOfKind(SpriteKind.Enemy)
-        for (let sprite of sprite_list) {
-            setFacing(sprite, img`
+        if (playerSprite.x < 0) {
+            playerSprite.setImage(img`
                 . . . . . . . . . . . . . . . . 
-                . . . . . . . . . . b 5 b . . . 
-                . . . . . . . . . b 5 b . . . . 
-                . . . . . . b b b b b b . . . . 
-                . . . . . b b 5 5 5 5 5 b . . . 
-                . b b b b b 5 5 5 5 5 5 5 b . . 
-                . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-                . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-                . . b d 5 5 b 1 f f 5 4 4 c . . 
-                b b d b 5 5 5 d f b 4 4 4 4 4 b 
-                b d d c d 5 5 b 5 4 4 4 4 4 b . 
-                c d d d c c b 5 5 5 5 5 5 5 b . 
-                c b d d d d d 5 5 5 5 5 5 5 b . 
-                . c d d d d d d 5 5 5 5 5 d b . 
-                . . c b d d d d d 5 5 5 b b . . 
-                . . . c c c c c c c c b b . . . 
-                `, img`
-                . . . . . . . . . . b 5 b . . . 
-                . . . . . . . . . b 5 b . . . . 
-                . . . . . . b b b b b b . . . . 
-                . . . . . b b 5 5 5 5 5 b . . . 
-                . . . . b b 5 d 1 f 5 d 4 c . . 
-                . . . . b 5 5 1 f f d d 4 4 4 b 
-                . . . . b 5 5 d f b 4 4 4 4 b . 
-                . . . b d 5 5 5 5 4 4 4 4 b . . 
-                . . b d d 5 5 5 5 5 5 5 5 b . . 
-                . b d d d d 5 5 5 5 5 5 5 5 b . 
-                b d d d b b b 5 5 5 5 5 5 5 b . 
-                c d d b 5 5 d c 5 5 5 5 5 5 b . 
-                c b b d 5 d c d 5 5 5 5 5 5 b . 
-                . b 5 5 b c d d 5 5 5 5 5 d b . 
-                b b c c c d d d d 5 5 5 b b . . 
-                . . . c c c c c c c c b b . . . 
-                `, img`
-                . . . . . . . . . . b 5 b . . . 
-                . . . . . . . . . b 5 b . . . . 
-                . . . . . . . . . b c . . . . . 
-                . . . . . . b b b b b b . . . . 
-                . . . . . b b 5 5 5 5 5 b . . . 
-                . . . . b b 5 d 1 f 5 5 d f . . 
-                . . . . b 5 5 1 f f 5 d 4 c . . 
-                . . . . b 5 5 d f b d d 4 4 . . 
-                b d d d b b d 5 5 5 4 4 4 4 4 b 
-                b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
-                b d c 5 5 5 5 d 5 5 5 5 5 b . . 
-                c d d c d 5 5 b 5 5 5 5 5 5 b . 
-                c b d d c c b 5 5 5 5 5 5 5 b . 
-                . c d d d d d d 5 5 5 5 5 d b . 
-                . . c b d d d d d 5 5 5 b b . . 
-                . . . c c c c c c c c b b . . . 
+                . . . . f f f f f f . . . . . . 
+                . . . f 2 f e e e e f f . . . . 
+                . . f 2 2 2 f e e e e f f . . . 
+                . . f e e e e f f e e e f . . . 
+                . f e 2 2 2 2 e e f f f f . . . 
+                . f 2 e f f f f 2 2 2 e f . . . 
+                . f f f e e e f f f f f f f . . 
+                . f e e 4 4 f b e 4 4 e f f . . 
+                . . f e d d f 1 4 d 4 e e f . . 
+                . . . f d d d d 4 e e e f . . . 
+                . . . f e 4 4 4 e d d 4 . . . . 
+                . . . f 2 2 2 2 e d d e . . . . 
+                . . f f 5 5 4 4 f e e f . . . . 
+                . . f f f f f f f f f f . . . . 
+                . . . f f f . . . f f . . . . . 
+                `)
+        } else if (playerSprite.x > 0) {
+            playerSprite.setImage(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . f f f f f f . . . . 
+                . . . . f f e e e e f 2 f . . . 
+                . . . f f e e e e f 2 2 2 f . . 
+                . . . f e e e f f e e e e f . . 
+                . . . f f f f e e 2 2 2 2 e f . 
+                . . . f e 2 2 2 f f f f e 2 f . 
+                . . f f f f f f f e e e f f f . 
+                . . f f e 4 4 e b f 4 4 e e f . 
+                . . f e e 4 d 4 1 f d d e f . . 
+                . . . f e e e 4 d d d d f . . . 
+                . . . . 4 d d e 4 4 4 e f . . . 
+                . . . . e d d e 2 2 2 2 f . . . 
+                . . . . f e e f 4 4 5 5 f f . . 
+                . . . . f f f f f f f f f f . . 
+                . . . . . f f . . . f f f . . . 
+                `)
+        } else {
+            playerSprite.setImage(img`
+                . . . . . . f f f f . . . . . . 
+                . . . . f f f 2 2 f f f . . . . 
+                . . . f f f 2 2 2 2 f f f . . . 
+                . . f f f e e e e e e f f f . . 
+                . . f f e 2 2 2 2 2 2 e e f . . 
+                . . f e 2 f f f f f f 2 e f . . 
+                . . f f f f e e e e f f f f . . 
+                . f f e f b f 4 4 f b f e f f . 
+                . f e e 4 1 f d d f 1 4 e e f . 
+                . . f e e d d d d d d e e f . . 
+                . . . f e e 4 4 4 4 e e f . . . 
+                . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+                . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+                . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+                . . . . . f f f f f f . . . . . 
+                . . . . . f f . . f f . . . . . 
                 `)
         }
     }
 })
 game.onUpdateInterval(1000, function () {
     gameSpeed += 5
-})
-game.onUpdateInterval(300, function () {
-    if (Math.percentChance(75)) {
-        music.playTone(notes[randint(0, notes.length - 1)], music.beat(BeatFraction.Half))
-    }
 })
